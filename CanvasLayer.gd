3d8acb4db909a40cdc4ce6963239d2df2fe2c5ef
John Rigler <john@rigler.org>
2024-05-18 19:40:24 -0500

    Initial commit https://forum.godotengine.org/t/networking-mixing-http-request-and-rpc-call/54014

---
+++ CanvasLayer.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _ready():
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
+
+func _on_Button_pressed():  
+	$HTTPRequest.request("https://httpbin.org/get?insertArbitraryDataHere")
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
+# Example from HTTPRequest Help
+# Outputs to Log File
