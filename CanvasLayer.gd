a625c5dcfd38591aec0273d7f4503f0734c75084
John Rigler <john@rigler.org>
2024-05-18 20:07:36 -0500

    Returns first result from polygonscan record

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -4,10 +4,11 @@ func _ready():
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 
 func _on_Button_pressed():  
-	$HTTPRequest.request("https://httpbin.org/get?insertArbitraryDataHere")
+	$HTTPRequest.request("https://api.polygonscan.com/api?module=account&action=txlist&address=0xF7e9857AFD27Ba03493BdC57F2588E71C976739f&sort=desc")
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
-	print(json.result)
+	print(json.result.result[0])
 # Example from HTTPRequest Help
+# Pulls back a big complex chunk of JSON
 # Outputs to Message Log
